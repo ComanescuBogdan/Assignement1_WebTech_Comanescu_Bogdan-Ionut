12aac46213840efd18e09c554643279ab36560f4
BogdanC <comanescubogdan20@stud.ase.ro>
2022-10-27 00:08:58 +0300

    All test pass

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 function hash(input, dictionary) {
 	//  TODO: implementați funcția
+	if(!(typeof input === 'string')){
+		throw new Error("Input should be a string");
+	}
+
+	for (let i = 0; i < dictionary.length ; i++) {
+		if(!(typeof dictionary[i] === 'string')){
+			throw new Error("Invalid dictionary format");
+		}
+	  } 
+
+	  let text=input;
+	  for (let i = 0; i < dictionary.length ; i++) {
+		let cuv = dictionary[i];
+		let poz = text.toLowerCase().search(cuv);
+		  if(poz!=-1)
+		  {
+			let diez="";
+		  	for(let u = 1;u<cuv.length-1;u++)	  diez = diez+"#";
+			if(text.at(poz)>= 'A' && text.at(poz)<='Z')
+			{
+				cuv = text.charAt(poz) + text.substring(poz+1,cuv.length)
+				text = text.replace(cuv,  cuv[0] + diez + cuv[cuv.length-1]);
+			}
+			else{
+				text = text.replace(cuv, cuv[0] + diez + cuv[cuv.length-1]);
+			}
+		  }
+		}   
+		return text;
+	
 	
 }
 
@@ -8,3 +38,4 @@ const app = {
 };
 
 module.exports = app;
+
